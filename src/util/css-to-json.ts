026062f382f986c73228c04cf2a896c6cf6301f8
Hunter Johnston <johnstonhuntera@gmail.com>
2023-06-24 17:37:52 -0400

    deps

--- src/util/css-to-json.ts
+++ src/util/css-to-json.ts
@@ -1,8 +1,7 @@
-import { CSSProperties } from "react";
-import postcss from "postcss";
-import postcssJs from "postcss-js";
+import postcss from "postcss"
+import postcssJs from "postcss-js"
 
-export function cssToJson(css: string): CSSProperties {
-  const root = postcss.parse(css);
-  return postcssJs.objectify(root);
+export function cssToJson(css: string) {
+	const root = postcss.parse(css)
+	return postcssJs.objectify(root)
 }
